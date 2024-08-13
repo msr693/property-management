e0d895f2e4b5ae149723bd8d8bfa9fea418101fb
msr693 <msr693@gmail.com>
2024-08-13 14:49:28 +0000

    user objects

--- src/main/java/com/mycompany/property_management/repository/UserRepository.java
+++ src/main/java/com/mycompany/property_management/repository/UserRepository.java
@@ -4,6 +4,8 @@ import com.mycompany.property_management.entity.PropertyEntity;
 import com.mycompany.property_management.entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<UserEntity,Long> {
 
 
